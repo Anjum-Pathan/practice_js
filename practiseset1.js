eeb90b98b97ba43dda2b1866ee46505b03efcac9
anjumpathan <anjumpathan966@gmail.com>
2023-01-08 20:31:28 +0530

    all question completed

--- practiseset1.js
+++ practiseset1.js
@@ -1,4 +1,29 @@
-// Create a variable of type string and try to add a number to it
+// cahpter-1 Q-1 Create a variable of type string and try to add a number to it..
 let name = "anjum";
 let number2 = 2;
 console.log(name + number2);
+
+// // cahpter-1 Q-2  use typeof operators to find the data types of the string in the last question?
+console.log(typeof (name + number2));
+//cahpter-1 Q-3 creat a const object in js can you change it to hold a num later?
+const a = {
+  name: iffat,
+  age: 23,
+};
+// a = "xyz"; // i cant change it to older string letter ..
+
+// cahpter-1 Q-4 try to add a new key to the const object in problem 3 were you  able to do it?
+const a1 = {
+  name: "anjum",
+  age: 24,
+};
+a1["name"] = "ifat";
+console.log(a1);
+// cahpter-1 Q-5 write a js program to create a word meaning dictonary of 5 word.
+
+const dict = {
+  anjum: "fy student",
+  nani: " student",
+  gulnaz: " ty student",
+};
+console.log(dict["gulnaz"]);
