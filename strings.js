b0c6f77e9a1642b3fa817d7954011d8db6f7267d
anjumpathan <anjumpathan966@gmail.com>
2023-01-12 19:05:16 +0530

    string basic complted

---
+++ strings.js
@@ -0,0 +1,26 @@
+let name = "Anjum";
+console.log(name);
+// for length count we need to write .length
+let name1 = "Anjum";
+console.log(name1.length);
+
+//string can also be created using single quotes
+let frnd = "iffat";
+console.log(frnd);
+
+//template literal
+let fruits5 = "anjum";
+let a = `this is ${fruits5}`;
+
+//escape sequence character.
+let fruits = "anjum \t pathan"; // op : anjum 	 pathan giving space between word
+console.log(fruits);
+
+let fruits1 = "anjum \n pathan";
+console.log(fruits1);
+// op : anjum
+//     pathan its come into new line
+
+let fruits3 = 'anjum " pathan';
+console.log(fruits3);
+//op :"anjum " pathan" extra double quotes
