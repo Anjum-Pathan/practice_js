8cc483b9ae9b9a1e36e43e8f0ca1269db660860e
anjumpathan <anjumpathan966@gmail.com>
2023-02-01 22:53:04 +0530

    prompt

--- basic/alert.js
+++ basic/alert.js
@@ -1,3 +1,9 @@
-alert("hii ");
-let notify = prompt("enter the num", "458");
-console.log("notify");
+// alert("hii ");
+// let notify = prompt("enter the num", "458");
+// console.log("notify");
+// window.console.log(window);
+// console.log(document.body);
+// document.body.style.background = "blue";
+
+//practise set
+//Q.1 write program using promt function to take input of age as a value from the user and use alert to tell him if  the can driveaaaaaaaaaaaaaaaaaaaaaaaaszs
