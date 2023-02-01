7973c12ac9f2a87ad81b6d210e2baf17ae675fde
anjumpathan <anjumpathan966@gmail.com>
2023-02-01 23:34:23 +0530

    Q2

--- basic/alert.js
+++ basic/alert.js
@@ -7,12 +7,25 @@
 
 //practise set
 //Q.1 write program using prompt function to take input of age as a value from the user and use alert to tell him if  the can drive.
+// let age = prompt("Enter your age");
+// Number.parseInt(age);
+// if (age <= 18) {
+//   alert("you age is less than 18 ");
+// } else {
+//   alert("you can drive");
+// }
+
+//Q2 in the Q1 use confirm to ask  user if he wants to see the prompt again
 let age = prompt("Enter your age");
 Number.parseInt(age);
-if (age <= 18) {
-  alert("you age is less than 18 ");
-} else {
-  alert("you can drive");
+while (runagain) {
+  const candrive = (age) => {
+    return age >= 18 ? true : false;
+  };
+  if (candrive(age)) {
+    alert("you age is less than 18 ");
+  } else {
+    alert("you can drive");
+  }
+  runagain = confirm("do you want to play again");
 }
-
-//Q2
