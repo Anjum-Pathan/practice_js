67ee3227cd5c82052c1a6eb74128b673dc02c4d4
anjumpathan <anjumpathan966@gmail.com>
2023-02-01 23:17:02 +0530

    q1

--- basic/alert.js
+++ basic/alert.js
@@ -6,4 +6,13 @@
 // document.body.style.background = "blue";
 
 //practise set
-//Q.1 write program using promt function to take input of age as a value from the user and use alert to tell him if  the can driveaaaaaaaaaaaaaaaaaaaaaaaaszs
+//Q.1 write program using prompt function to take input of age as a value from the user and use alert to tell him if  the can drive.
+let age = prompt("Enter your age");
+Number.parseInt(age);
+if (age <= 18) {
+  alert("you age is less than 18 ");
+} else {
+  alert("you can drive");
+}
+
+//Q2
