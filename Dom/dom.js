0d39efeb0acd548d79700c03515bae361be0a812
anjumpathan <anjumpathan966@gmail.com>
2023-02-08 01:15:27 +0530

    Q3

--- Dom/dom.js
+++ Dom/dom.js
@@ -1,5 +1,12 @@
-// Q1. create navbar change the color of its first element to red
+// Q1: create navbar change the color of its first element to red
 document.getElementsByTagName("nav-list")[0].firstElementChild.style.color =
   "blue";
-//create table without using tbody now use "view page source" button to check whether it has a tbody or not
-n;
+//Q2: create table without using tbody now use "view page source" button to check whether it has a tbody or not
+// ans is No
+
+//Q3.create an element with 3 childeren and first and last elemt to green
+document.getElementsByTagName(".nav-bar")[0].firstElementChild.style.color =
+  "green";
+document.getElementsByTagName(".nav-bar")[0].lastElementChild.style.color =
+  "green";
+//
