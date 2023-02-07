cbf2a6994c452a736ce0e4315b1bc8741e9ad36f
anjumpathan <anjumpathan966@gmail.com>
2023-02-08 01:01:30 +0530

    q1

--- Dom/dom.js
+++ Dom/dom.js
@@ -1,1 +1,5 @@
-// create navbar change the color of its first element to red
+// Q1. create navbar change the color of its first element to red
+document.getElementsByTagName("nav-list")[0].firstElementChild.style.color =
+  "blue";
+//create table without using tbody now use "view page source" button to check whether it has a tbody or not
+n;
